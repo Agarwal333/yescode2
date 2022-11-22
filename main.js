77ca806e85ae9c4b2f5d362d9c527dc619d1f3d0
Lakhan <lakhanagarwal369@gmail.com>
2022-11-22 00:04:40 -0500

    donation and stories page added

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-// class MainHeader extends HTMLElement{
-//     connectedCallBack(){
-//         this.innerHTML = `
-//             <header>
-//             <div class="logo">
-//                 <a href="#"><img src="images/logo.png" alt="leaf logo"/></a>
-//             </div>
-//             <nav>
-//                 <ul id="links">
-//                     <li><a href="takeAction.html">Take Action</a></li>
-//                     <li><a href="#">About Us</a></li>
-//                     <li><a href="#">Learning Centre</a></li>
-//                     <li><a href="#">Donation</a></li>
-//                     <img src="images/close.png" alt="close icon" onclick="closeMenu()"/>     
-//                 </ul>
-//                 <img src="images/open.png" alt="open icon" onclick="openMenu()"/>
-//             </nav>    
-//             <button class="loginBtn">Login</button>
-//             </header>
-//         `
-//     }
-// }
 
-// customElements.define("main-header", MainHeader);
 
 
 let links = document.getElementById("links");
@@ -33,8 +10,36 @@ let links = document.getElementById("links");
           }
 
 
-// let bg = getElementById("bg");
-//  window.addEventListener("scroll", function(){
-//     var value = window.scrollY;
-//     bg.style.top = value * 1.5 + "px";
-//  })
+
+
+// const reveal = document.querySelectorAll(".imageScroll");
+
+// window.addEventListener("scroll", checkboxes);
+// checkboxes();
+// function checkboxes(){
+//     const triggerBottom = window.innerHeight/ 5 * 4;
+// reveal.froEach((image)=>{
+//     const imageTop = image.getBoundingClientRect().top;
+
+//     if(imageTop < triggerBottom){
+//         image.classList.add("show");
+//     }else{
+//         image.classList.remove("show");
+//     }
+// })
+
+// }
+
+
+let container = document.querySelector(".helpContainer1");
+let layer = document.querySelectorAll(".layer");
+
+container.onmousemove = function(e){
+    let X = e.pageX;
+    let Y = e.pageY;
+
+    layer[0].style.transform = 'translate('+ X/100*2 +'px, '+ Y/100*2 +'px)';
+    layer[1].style.transform = 'translate('+ X/100*2 +'px, '+ Y/100*2 +'px)';
+    layer[2].style.transform = 'translate('+ X/100*4 +'px, '+ Y/100*4 +'px)';
+
+}
